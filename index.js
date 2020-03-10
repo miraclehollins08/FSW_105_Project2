8728e86e14e3008931de60348df4711a2b4b9c24
Miracle <54377927+miraclehollins08@users.noreply.github.com>
2020-03-09 21:05:00 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function addingTwoInputNumber(num1, num2)
+{
+    return (num1 + num2);
+}
+
+console.log(addingTwoInputNumber(24, 2));
+
+function multipliesTwoInputNumber(num2, num3)
+{
+    return (num2 * num3);
+}
+
+console.log(multipliesTwoInputNumber(7, 9));
+
+function divideTwoInputNumber (num4, num5)
+{
+    return (num4 / num5)
+}
+
+console.log(divideTwoInputNumber(10, 5));
+
+function subtractTwoInputNumber(num6, num7)
+{
+    return (num6 - num7)
+}
+
+console.log(subtractTwoInputNumber(32, 17));
